b78eed2e6aa2a21ffe2ee5e42058f1ad488045ed
jacobhorne09 <139819469+jacobhorne09@users.noreply.github.com>
2025-02-24 19:24:02 -0800

    Completed the main method with the outer shell of my interpreter that creates an interpreter engine and runs it

--- project3.py
+++ project3.py
@@ -1,16 +1,6 @@
-# project3.py
-#
-# ICS 33 Winter 2025
-# Project 3: Why Not Smile?
-#
-# The main module that executes your Grin interpreter.
-#
-# WHAT YOU NEED TO DO: You'll need to implement the outermost shell of your
-# program here, but consider how you can keep this part as simple as possible,
-# offloading as much of the complexity as you can into additional modules in
-# the 'grin' package, isolated in a way that allows you to unit test them.
-
 from grin.parsing import parse
+from grin.parser import parse_statements_into_objects
+from grin.engine import InterpreterEngine
 
 
 def main() -> None:
@@ -23,6 +13,11 @@ def main() -> None:
             statement_list.append(statement)
 
         parsed_statements = list(parse(statement_list))
+        parsed_objects = parse_statements_into_objects(parsed_statements)
+        engine = InterpreterEngine(parsed_objects)
+        engine.run()
+
+
     except Exception as e:
         print(str(e))
 
